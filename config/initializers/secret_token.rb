215add06f450a170eab8fb41b1b88c4f79998077
Tagaki Aki <a-tagaki@esm.co.jp>
2011-08-07 17:06:14 +0900

    Head First Rails chapter 2.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mebay::Application.config.secret_token = '7697f302e2f353348d3283a57d6ccbb7b65687b62fb0002fef7b81ff0c3c4fb165c3724d94df28efa36beae425d21091a2e25ee1f8be4ee9c979fa73a2268c14'
